3bfade918c7645f8a93e15de15430906489bf27c
Sergei Ampilov <sergei.ampilov@gmail.com>
2020-03-19 19:10:04 +0300

    'v2'

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ var b = [1, 'hello', 32];
 
 
 Array.prototype.double = function () {
-    // console.log(this);
+    
+
+
+    return this.map(function(element){
+        if (typeof(element) === 'number') {
+            return element * element;
+        } else if (typeof(element) === 'string') {
+            return element + element;
+        }
+
+        return element;
+    });
 
     var res = [];
 
     this.forEach(element => {
         console.log(typeof(element));
-        if (typeof(element) === 'number') {
-            res.push(element * element);
-        } else if (typeof(element) === 'string') {
-            res.push(element + element);
-        } else {
-            res.push(element);
-        }
+        
     });
 
     return res;
